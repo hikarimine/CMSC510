9d4dfb1418d82193370a8529b1c2bb1ecba44411
Hikari Mine <hikari.mine@lawrence.edu>
2018-10-05 22:47:24 -0500

    Modified methods

--- src/MissionariesandCannibals/MissionariesAndCannibalsBoard.java
+++ src/MissionariesandCannibals/MissionariesAndCannibalsBoard.java
@@ -11,17 +11,19 @@ public class MissionariesAndCannibalsBoard {
 
     private int[] state;
     
+    public static Action moveMM = new DynamicAction("moveMM");
+    public static Action moveCC = new DynamicAction("moveCC");
+    public static Action moveC = new DynamicAction("moveC");
+    public static Action moveM = new DynamicAction("moveM");
+    public static Action moveMC = new DynamicAction("moveMC");
     public static Action moveB = new DynamicAction("moveB");
-    public static Action cBoard = new DynamicAction("cBoard");
-    public static Action mBoard = new DynamicAction("mBoard");
-    public static Action cDebark = new DynamicAction("cDebark");
-    public static Action mDebark = new DynamicAction("mDebark");
     
-    
-// State rep: [wolf, goat, cabbage, boat] (0=L)
+// State rep: [LeftC,LeftM,RightC,RightM,Boat] 
 // Default constructor starts everyone on the left bank.
     public MissionariesAndCannibalsBoard() {
-        state = new int[]{3, 3, 0, 0, 0};
+        state = new int[]{0, 0, 0, 0, 0};
+        //start [3,3,0,0,0]
+        //end [0,0,3,3,1]
     }
 // Construct a board based on the array representation to copy.
     public MissionariesAndCannibalsBoard(int[] state) {
@@ -40,96 +42,109 @@ public class MissionariesAndCannibalsBoard {
 
 // Update the state based on the action.
 // Only  gets called if move is for sure legal
-    public void moveB() {
+     public void moveB() {
         if (state[4] == 0){
             state[4] = 1;
         }else{
             state[4] = 0;
-        }             
+        }   
     }
-
-    public void cBoard() {
-        if (state[4] == 0){
-            state[0] --;
-        }else{
-            state[2] --;
-        }           
-    }
-
-    public void mBoard() {
-        if (state[4] == 0){
-            state[1] --;
-        }else{
-            state[3] --;
-        }          
-    }
-
-    public void cDebark() {
-        if (state[4] == 0){
-            state[0] ++;
-        }else{
-            state[2] ++;
+    public void moveMM() {
+        if(state[1] >= 2){ // MM move from left to right
+            state[1] = state[1] - 2;
+            state[3] = state[3] + 2;
+        }else{ //MM move from right to left
+            state[1] = state[1] + 2;
+            state[3] = state[3] - 2;
         }
     }
-    
-    public void mDebark() {
-        if (state[4] == 0){
-            state[1] ++;
-        }else{
-            state[3] ++;
+    public void moveCC() { 
+        if(state[0] >= 2){ // CC move from left to right
+            state[0] = state[0] - 2;
+            state[2] = state[2] + 2;
+        }else{ // CC move from right to left
+            state[0] = state[0] + 2;
+            state[2] = state[2] - 2;
         }
     }
-
+    public void moveMC() { 
+        if(state[1] >= 1 && state[0] >= 1){ // M and C move from left to right
+            state[1] = state[1] - 1;
+            state[0] = state[0] - 1;
+            state[2] = state[2] + 1;
+            state[3] = state[3] + 1;
+        }else{  // M and C move from right to left
+            state[1] = state[1] + 1;
+            state[0] = state[0] + 1;
+            state[2] = state[2] - 1;
+            state[3] = state[3] - 1;
+        }
+    }
+    public void moveM(){  
+        if(state[1] >= 1){  // M move from left to right
+            state[1] = state[1] - 1;
+            state[3] = state[3] + 1;
+        }else{  // M move from right to left
+            state[1] = state[1] + 1;
+            state[3] = state[3] - 1;
+        }
+    }
+    public void moveC(){
+        if(state[0] >= 1){  // C move from left to right
+            state[0] = state[0] - 1;
+            state[2] = state[2] + 1;
+        }else{  // C move from right to left
+            state[0] = state[0] + 1;
+            state[2] = state[2] - 1;
+        }
+    }
 // This implements the logic for whether the Action object is a valid move.
-// This code uses (a+b+c)%3 a lot. Since a,b,c all either 0 or 1,
-// (a+b+c)%3 = 0 iff a=b=c.
     public boolean isValidMove(Action where) {
-        if (where.equals(moveB)){
-            return isBoatEmpty();            
-        }else if (where.equals(cBoard)){
-            return !isBoatFull();
-        }else if (where.equals(mBoard)){
-            if (state[4] == 0){
-                if (state[0] == state[1]){
-                    return false;
-                }else{
-                    return !isBoatFull();
-                }
-            }else{
-                if (state[2] == state[3]){
-                    return false;
-                }else{
-                    return !isBoatFull();
-                }
-            }
-        }else if (where.equals(cDebark)){
-            if (state[4] == 0){
-                return !(state[0] == state[1]);
-            }else{
-                return !(state[2] == state[3]);
-            }
-        }else if (where.equals(mDebark)){
-            return true;
-        }
-        return false; //if the move given isn't one of the five options
-    }
-    
-    private boolean isBoatEmpty(){
-        return (state[0] + state[2] < 3) || (state[1] + state[3] < 3);
-    }
-    
-    private boolean isBoatFull(){
-        if (state[0] + state[2] == 1){
-            return true;
-        }else if (state[1] + state[3] == 1){
-            return true;
-        }else if ((state[0] + state[2] == 2) && (state[1] + state[3] == 2)){
-            return true;
-        }else{
+        if (where.equals(moveMM)){ // move MM to another side
+            // MM and B are both on left
+            if (state[1] >= 2 && state[4] == 0){ 
+                // check that there are equal or more Ms than Cs before and after the move
+                return ((state[1] - 2) >= state[0])&&((state[3] + 2) >= state[2]);
+            } else if(state[3] >= 2 && state[4] == 1){ // check that MM and B are both on right
+                return ((state[3] - 2) >= state[2])&&((state[1] + 2) >= state[0]);
+            } else 
+                return false;
+        }else if (where.equals(moveCC)){
+            if (state[0] >= 2 && state[4] == 0){ //check that CC and B are both on left
+                // check that there are equal or more Ms than Cs before and after the move
+                return ((state[0] - 2) <= state[1])&&((state[2] + 2) <= state[3]);
+            } else if(state[2] >= 2 && state[4] == 1){ // check that CC and B are both on right
+                return ((state[2] - 2) <= state[3])&&((state[0] + 2) <= state[1]);
+            } else 
+                return false;
+        }else if (where.equals(moveM)){ // moving M from one side to another
+            if (state[1] >= 1 && state[4] == 0){ //check that M and B are both on left
+                // check that there are equal or more Ms than Cs before and after the move
+                return ((state[1] - 1) >= state[0])&&((state[3] + 1) >= state[2]);
+            } else if(state[3] >= 1 && state[4] == 1){ // check that M and B are both on right
+                return ((state[3] - 1) >= state[2])&&((state[1] + 1) >= state[0]);
+            } else 
+                return false;
+        }else if (where.equals(moveC)){ // moving C from one side to another
+            if (state[0] >= 1 && state[4] == 0){ //check that C and B are both on left
+                // check that there are equal or more Ms than Cs before and after the move
+                return ((state[0] - 1) <= state[1])&&((state[2] + 1) <= state[3]);
+            } else if(state[2] >= 1 && state[4] == 1){ // check that C and B are both on right
+                return ((state[2] - 1) <= state[3])&&((state[0] + 1) <= state[1]);
+            } else 
+                return false;
+        }else if(where.equals(moveMC)){
+            if(state[0] > 0 && state[1] > 0 && state[4] == 0){ // at least one C and on M on left side
+                // check that there are euqal or more Ms than Cs before and after the move
+                return ((state[0] - 1) <= (state[1] - 1)) && (state[2] + 1) <= (state[3] + 1);
+            } else if(state[2] > 0 && state[3] > 0 && state[4] == 1){ // at least one C and M on right side
+                return ((state[2] - 1) <= (state[3] - 1)) && (state[0] + 1) <= (state[1] + 1);
+            } else
+                return false;
+        }else
+        // todo: add the case for moveB, think about the case when boat can/cannot move, otherwise NO SOLUTION FOUND
             return false;
-        }
     }
-
     @Override
     public int hashCode() {     // Where is this used?
         final int prime = 31;
@@ -169,7 +184,8 @@ public class MissionariesAndCannibalsBoard {
         if(state[1] == 3) s += "M M M ";
         if (state[4] == 0) {
             s += "BOAT --RIVER--      ";   
-        } else {
+        } 
+        if(state[4] == 1) {
             s += "      --RIVER-- BOAT ";
         }
         if(state[2] == 0) s += " ";
